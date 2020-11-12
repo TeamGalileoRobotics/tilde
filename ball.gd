9bf77cac94c9b6221c6fd6e27764cd4dec16379a
GalileoRobotics <info@galileorobotics.ch>
2020-11-12 19:31:12 +0100

    Add speed variable

--- ball.gd
+++ ball.gd
@@ -1,13 +1,12 @@
 extends RigidBody2D
 
-const SPEED = 20
-var impulse = Vector2(0.3,0.3)
+export var speed = 20
 export var index = 1
 
 func _ready():
 	set_process(true)
 
 func _physics_process(_delta):
-	var playerdata = get_parent().get_data(index)
-	impulse = Vector2(playerdata[0], -playerdata[1])
-	set_applied_force(impulse*SPEED)	
+	var data = get_parent().get_data(index)
+	var impulse = Vector2(data[0], -data[1])
+	set_applied_force(impulse*speed)
