8f3bf2e01fc8abeacd28951c2f5d80df7694d2ee
GalileoRobotics <info@galileorobotics.ch>
2020-12-03 19:38:13 +0100

    Implement smooth zoom

--- camera.gd
+++ camera.gd
@@ -1,5 +1,10 @@
 extends Camera2D
 
+export var min_zoom = 0.1
+export var max_zoom = 1
+export var margin = 150
+export var zoom_smoothness = 5
+
 var players
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +15,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Calculate bbox center
-	var min_x = 1e9
-	var min_y = 1e9
-	var max_x = -1e9
-	var max_y = -1e9
+	var min_x = INF
+	var min_y = INF
+	var max_x = -INF
+	var max_y = -INF
 	
 	for player in players:
 		min_x = min(min_x, player.position.x)
@@ -21,4 +26,11 @@ func _process(delta):
 		max_x = max(max_x, player.position.x)
 		max_y = max(max_y, player.position.y)
 	
-	self.offset = Vector2((min_x + max_x) / 2.0, (min_y + max_y) / 2.0)
+	self.position = Vector2((min_x + max_x) / 2.0, (min_y + max_y) / 2.0)
+	
+	# Calculate zoom level
+	var screen_size = self.get_viewport_rect().size
+	var x_fac = (max_x - min_x + margin) / screen_size.x
+	var y_fac = (max_y - min_y + margin) / screen_size.y
+	var val = max(min(max(x_fac, y_fac), max_zoom), min_zoom)
+	self.zoom = self.zoom.linear_interpolate(Vector2(1, 1) * val, zoom_smoothness * delta)
